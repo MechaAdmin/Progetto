caa9dc936e7fbe1dc56356bfbfa748ad2942cd49
tomma <MechaShopping@gmail.com>
2016-04-13 10:36:26 +0200

    o

--- app/src/main/java/com/example/riccardo/hermes/Vendita.java
+++ app/src/main/java/com/example/riccardo/hermes/Vendita.java
@@ -5,6 +5,8 @@ import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -64,8 +66,8 @@ public class Vendita extends Fragment {
                 startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
             }
         });
-        btnInsVendita.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        btnInsVendita.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 nomeProdotto = txtNome.getText().toString();
                 prezzo = txtPrezzo.getText().toString();
                 descrizione = txtDescrizione.getText().toString();
@@ -84,7 +86,8 @@ public class Vendita extends Fragment {
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), filePath);
 
-                immagine.setImageBitmap(resizeImage(convertToMutable(bitmap)));
+
+                immagine.setImageBitmap(resizeImage(bitmap));
             } catch (IOException e) {
                 Toast.makeText(getActivity(),e.getMessage(),Toast.LENGTH_SHORT).show();
             }
@@ -119,89 +122,46 @@ public class Vendita extends Fragment {
             @Override
             protected String doInBackground(Bitmap... params) {
                 Bitmap bitmap = params[0];
-                String uploadImage = getStringImage(bitmap);
+            String uploadImage = getStringImage(bitmap);
 
-                HashMap<String,String> data = new HashMap<>();
+            HashMap<String,String> data = new HashMap<>();
 
-                data.put("immagine", uploadImage);
-                data.put("nomeProdotto",nomeProdotto);
-                data.put("prezzo",prezzo);
-                data.put("descrizione",descrizione);
-                data.put("venditore",username);
-                String result = rh.sendPostRequest("http://mechavendor.16mb.com/uploadProdotti.php",data);
+            data.put("immagine", uploadImage);
+            data.put("nomeProdotto",nomeProdotto);
+            data.put("prezzo",prezzo);
+            data.put("descrizione",descrizione);
+            data.put("venditore",username);
+            String result = rh.sendPostRequest("http://mechavendor.16mb.com/uploadProdotti.php",data);
 
-                return result;
-            }
+            return result;
         }
-
-        UploadDati ui = new UploadDati();
-        ui.execute(bitmap);
     }
-   public static Bitmap resizeImage(Bitmap image){
+
+    UploadDati ui = new UploadDati();
+    ui.execute(bitmap);
+}
+
+    public static Bitmap resizeImage(Bitmap image){
         double height = image.getHeight();
         double width = image.getWidth();
+        double maxTexture = 2048;
 
 
+        if(width > maxTexture || height > maxTexture) {
+            if(width > height ){
+                double scale = width - maxTexture;
+                scale = scale / width;
+                height = height - height * scale;
+                width = maxTexture;
 
-        if(height > 4096){
-            double scale = height - 4096;
-            scale = scale / height;
-            width = width - width * scale;
-            image.setHeight(4096);
-            image.setWidth((int)width);
+            }else{
+                double scale = height - maxTexture;
+                scale = scale / height;
+                width = width - width * scale;
+                height = maxTexture;
+            }
         }
-        if(width > 4096){
-            double scale = width - 4096;
-            scale = scale / width;
-            height = height - height * scale;
-            image.setHeight((int)height);
-            image.setWidth(4096);
-        }
-        return image;
+        return Bitmap.createScaledBitmap(image, (int)width, (int)height, true);
     }
-    public static Bitmap convertToMutable(Bitmap imgIn) {
-        try {
-            //this is the file going to use temporally to save the bytes.
-            // This file will not be a image, it will store the raw image data.
-            File file = new File(Environment.getExternalStorageDirectory() + File.separator + "temp.tmp");
 
-            //Open an RandomAccessFile
-            //Make sure you have added uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"
-            //into AndroidManifest.xml file
-            RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
-
-            // get the width and height of the source bitmap.
-            int width = imgIn.getWidth();
-            int height = imgIn.getHeight();
-            Bitmap.Config type = imgIn.getConfig();
-
-            //Copy the byte to the file
-            //Assume source bitmap loaded using options.inPreferredConfig = Config.ARGB_8888;
-            FileChannel channel = randomAccessFile.getChannel();
-            MappedByteBuffer map = channel.map(FileChannel.MapMode.READ_WRITE, 0, imgIn.getRowBytes()*height);
-            imgIn.copyPixelsToBuffer(map);
-            //recycle the source bitmap, this will be no longer used.
-            imgIn.recycle();
-            System.gc();// try to force the bytes from the imgIn to be released
-
-            //Create a new bitmap to load the bitmap again. Probably the memory will be available.
-            imgIn = Bitmap.createBitmap(width, height, type);
-            map.position(0);
-            //load it back from temporary
-            imgIn.copyPixelsFromBuffer(map);
-            //close the temporary file and channel , then delete that also
-            channel.close();
-            randomAccessFile.close();
-
-            // delete the temp file
-            file.delete();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return imgIn;
-    }
 }
